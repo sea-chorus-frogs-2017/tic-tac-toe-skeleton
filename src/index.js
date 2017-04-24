1a2c42d1e7239d749537d76de9d944b7dd8ac5d1
Eitan Greene & Julia(e) Xu <pair+eitan-g.jmxu94@devbootcamp.com>
2017-04-24 16:20:41 -0700

    Create onClick event listener for square and onClick prop for square

--- src/index.js
+++ src/index.js
@@ -11,16 +11,22 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
   render() {
     const status = 'Next player: X';
