9433e5cbaf3a4bcccc6482ed1667d3134a4d2461
Eitan Greene & Julia(e) Xu <pair+eitan-g.jmxu94@devbootcamp.com>
2017-04-24 15:30:58 -0700

    Gives square correct props

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {this.props.length}
+        {this.props.value}
       </button>
     );
   }
@@ -14,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} potato="french" />;
+    return <Square value={i} />;
   }
   render() {
     const status = 'Next player: X';
