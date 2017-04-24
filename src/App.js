d39d9b5b8a5db6b5369fe7ef1c2b4623e733330c
Dan McKeon & Nick Jarin <pair+danmckeon.njarin@devbootcamp.com>
2017-04-24 15:51:11 -0700

    write handleClick in Game and delete comments

--- src/App.js
+++ src/App.js
@@ -63,6 +63,23 @@ class Game extends React.Component {
       xIsNext: true
     };
   }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
