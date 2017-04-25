26b59ee9ed350d575f07160949ce3c2565024d1b
Eitan Greene & Julia(e) Xu <pair+eitan-g.jmxu94@devbootcamp.com>
2017-04-24 17:57:17 -0700

    SAVE POINT: Create tic tac toe mvp functionality

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -37,7 +40,6 @@ class Board extends React.Component {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
         <div className="status">{status}</div>
